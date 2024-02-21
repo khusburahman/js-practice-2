9e2e72ac9dc47ce881f690fe92e5aedcf17a9d99
khusburahman <khusburahman49@gmail.com>
2024-02-21 21:42:18 +0600

    js done

---
+++ practice 1/index.js
@@ -0,0 +1,101 @@
+/*area of various shape*/
+// var base=prompt("Ener base= ");
+// var height=prompt("Enter height= ");
+// var area=base+height;
+// document.write("area ="+area);
+
+
+/*tempareture make convert*/
+// var farn=parseFloat(prompt("Enter farenhite="));
+// var cels=(farn-32)*(5/9);
+// document.write( "celsius "+cels);
+
+// /*letter grade*/
+// var marks=prompt("Enter marks:");
+//  if(marks>=80 && marks<=100)
+//     document.write("A+");
+// else if(marks>=70 && marks<=89)
+//     document.write("A");
+// else if(marks>=60 && marks<=69)
+//      document.write("A-");
+//  else
+//     document.write("Fail");
+
+
+
+/*digit spelling*/
+// var digit=prompt("enter digit:");
+//  if(digit==0)
+//     document.write("zero");
+//  else if(digit==1)
+//     document.write("one");
+// else if(digit==2)
+//     document.write("two");
+// else if(digit==3)
+//     document.write("three");
+//  else if(digit==4)
+//     document.write("four");
+//  else if(digit==5)
+//     document.write("five");
+// else if(digit==6)
+//     document.write("six");
+// else if(digit==7)
+//     document.write("seven");
+// else
+//     document.write("not a digit");
+
+
+
+// var digit=prompt("enter digit:");
+// if(digit==0)
+// document.write("zero");
+// else if(digit==1)
+// document.write("one");
+// else
+// document.writw("not digit");
+
+
+/*for loop*/
+
+// for(var x=1; x<=10; x=x+1){
+//     document.write("khusbu");
+// }
+
+// for(var x=1;x<=100;x=x+1){
+//    document.write(+x);
+// }
+
+/*even*/
+// for(var x=2;x<=100;x=x+2){
+//     document.write(+x);
+// }
+
+/*10-1 print*/
+// for(x=10;x>=1;x=x-1){
+//     document.write(+x);
+//}
+
+
+/*1+2+3+4+5*/
+// var sum=0;
+// for(var x=1;x<=5; x=x+1){
+//     sum=sum+x;
+//     document.write(sum);
+// }
+
+/*m-n sum*/
+// var m=parseInt(prompt("Enter the starting number:"));
+// var n=parseInt(prompt("Enter the last number:"));
+// for(x=m;x<=n;x=x+1){
+//     sum=sum+x;
+// }
+//     document.write(sum);
+
+
+/*sum between 2 numbers*/
+for(var x=1;x<=3;x++){
+    var num1=parseInt(prompt("enter 1st number:"));
+    var num2=parseInt(prompt("Enter last number:"));
+    var sum=sum1+sum2;
+    document.write(sum);
+}
---
+++ practice-2/index.js
@@ -0,0 +1,269 @@
+/*----------------------
+1/positive or negative
+--------------------*/
+// let x=0;
+// if(x>0){
+//     console.log(x,"positive number");
+// }
+// else if(x<0){
+//     console.log(x,"negative number");
+// }
+// else{
+//     console.log("zero");
+// }
+
+// let a=-43;
+// a>0 ? console.log("positive number"): console.log("negative number");
+
+
+/*---------------
+2/odd or even
+--------------*/
+// let a=45;
+// if(a%2==0){
+//     console.log(a,"even number");
+// }
+// else{
+//     console.log(a,"is odd number");
+// }
+
+
+/*----------------------
+3/max between two number
+----------------------*/
+// let a=34;
+// let b=14;
+// if(a>b){
+//     console.log("a is max number");
+// }
+// else if(a==b){
+//     console.log("both is equal");
+// }
+// else{
+//     console.log("b is max number");
+// }
+
+
+
+/*--------------------
+4/max between 3 number
+---------------------*/
+// let a=93;
+// let b=49;
+// let c=46;
+// if(a>b&&a>c){
+//     console.log("a is max number");
+// }
+// else if(b>a&&b>c){
+//     console.log("b is max number");
+// }
+// else{
+//     console.log("c is big number");
+// }
+
+
+/*week number and console total days*/
+
+// let week=7;
+// console.log(week*5,"days");
+
+// let year=12;
+// console.log(year*2,"month");
+
+
+/*----------------
+5/vowel-consonant
+----------------*/
+// let letter="o";
+// switch(letter){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log("vowel");
+//         break;
+//     default:
+//         console.log("consonant");
+// }
+
+
+
+/*-----------
+10/current bill
+-------------*/
+// let unit=25;
+// let additional_meter_charge=55;
+// if(unit<=50){
+//     let par_unit=.50;
+//     let result=(unit*par_unit)+additional_meter_charge;
+//     console.log(result);
+// }
+// else if(unit<=100){
+//     let par_unit=.75;
+//     let result=(unit*par_unit)+additional_meter_charge;
+//     console.log(result);
+// }
+// else if(unit<=250){
+//     let par_unit=1.20;
+//     let result=(unit*par_unit)+additional_meter_charge;
+//     console.log(result);
+// }
+// else{
+//     let par_unit=1.50;
+//     let result=(unit*par_unit)+additional_meter_charge;
+//     console.log(result);
+// }
+
+
+/*--------------------------------------------------------------
+7/input month number and console number of days in that month
+// --------------------------------------------------------------*/
+// let year=2021
+// let month=2
+// let feb=(year%4==0 && year%100!==0)||(year%400==0)?29:28
+// if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
+//     console.log(31);
+// }
+// else if(month==4 || month==6 || month==9 || month==11){
+//     console.log(30);
+// }
+// else if(month==2){
+//     console.log(feb);
+
+// }
+// else{
+//     console.log("no month");
+// }
+
+
+
+
+
+/*-------------------
+9/Result sheet
+// -----------------------*/
+// let marks=33;
+// if(marks>=80){
+//     console.log("A+");
+// }
+// else if(marks>=70){
+//     console.log("A");
+// }
+// else if(marks>=60){
+//     console.log("A-");
+// }
+// else if(marks>=50){
+//     console.log("B");
+// }
+// else if(marks>=40){
+//     console.log("C");
+// }
+// else{
+//     console.log("F");
+// }
+
+
+
+/*-----------------------
+8/number of day in a date
+// -------------------------*/
+// let year=2001
+// let month=2
+// let day=8
+// totalDays=0
+// let feb=(year%4==0 && year%100!==0)||(year%400==0)?29:28
+// if(month==1){
+//     totalDays=day
+// }
+// else if(month==2){
+//     totalDay=31+day
+// }
+// else if(month==3){
+//     totalDay=31+feb+day
+// }
+
+// console.log(totalDay);
+
+
+
+
+/*loop part*/
+/*--------------------
+caculate the avarage marks
+// --------------------*/
+// let math=75.25
+// let biology=56
+// let chemestry=80
+// let physics=35.45
+// let bangla =99.5
+// let totalMarks=(math+biology+chemestry+physics+bangla)/5
+// console.log(totalMarks.toFixed(2));
+
+
+
+// const numbers=[12,34,56,78,45,23,67,89,14,5];
+// numbers.forEach(number=>{
+//     if(number<50){
+//         console.log(number);
+//     }
+// });
+
+
+/*--------------
+odd number
+// -------------*/
+// for(let i=1;i<=99;i=i+2){
+//     console.log(i);
+// }
+
+// for(let x=2;x<=100;x=x+2){
+//     console.log(x);
+// }
+/**--------------------
+ while loop
+ --------------------*/
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// let x=10;
+// while(x>=40){
+//     console.log(x);
+//     i++
+// }
+
+
+/* -----------------
+do while loop
+------------------*/
+// let i=10
+// do{
+//     console.log(i)
+// }
+// while(0<=10);
+
+
+
+// let pi=Math.PI
+// console.log(pi.toFixed(2));
+
+
+// console.log(Math.random());
+// console.log(Math.floor());
+// console.log(Math.sign());
+// console.log(Math.ceil());
+
+
+
+// console.log(Math.round(Math.random()*255));
+
+
+
+// let r=Math.round(Math.random()*255);
+// let g=Math.round(Math.random()*255);
+// let b=Math.round(Math.random()*255);
+// let rgb=`rgb(${r},${g},${b})`
+// console.log(rgb);
